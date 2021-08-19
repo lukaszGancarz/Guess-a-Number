e2c541513b841dcebb29b3ca7eabd5e2f005020a
lukaszGancarz <lukasz.gancarz@wp.pl>
2021-08-19 12:35:17 +0200

    logic finished

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds <= 0)
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  else if (guessRounds > 0)
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
+
   return (
     <View style={styles.screen}>
       <StatusBar translucent backgroundColor="transparent" />
       <Header title="Guess a number!" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
@@ -17,6 +51,6 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    width: '100%'
+    width: "100%",
   },
 });
