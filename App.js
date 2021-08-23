c23b92b7128197536ce62d6d34cee19afd2315cb
lukaszGancarz <lukasz.gancarz@wp.pl>
2021-08-23 16:52:34 +0200

    images and text styling implemented

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import * as Font from 'expo-font';
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 // import { AppLoading } from 'expo';
-import AppLoading from 'expo-app-loading';
 
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
@@ -12,8 +12,8 @@ import GameOverScreen from "./screens/GameOverScreen";
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
 };
 
@@ -47,6 +47,16 @@ export default function App() {
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  
+  content = (
+    <GameOverScreen
+      roundsNumber={1}
+      userNumber={1}
+      onRestart={configureNewGameHandler}
+    />
+  );
+
 
   if (userNumber && guessRounds <= 0)
     content = (
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,21 +1,67 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, Image, StyleSheet, Button } from "react-native";
+import Colors from "../constants/colors";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
-const GameOverScreen = props => {
-    return <View style={styles.screen}>
-        <Text>The Game is Over!</Text>
-        <Text>Number of Rounds: {props.roundsNumber}</Text>
-        <Text>Number was: {props.userNumber}</Text>
-        <Button title="NEW GAME" onPress={props.onRestart}/>
+const GameOverScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          fadeDuration={1000}
+          style={styles.image}
+          resizeMode="cover"
+          // source={require("../assets/success.png")}
+          source={{
+            uri: "https://media.istockphoto.com/photos/hiker-man-at-the-top-of-the-mountainthis-is-a-3d-render-illustration-picture-id1089729132?k=6&m=1089729132&s=612x612&w=0&h=6N3db-V2cpUjvZlrT1wIXThCnmCbTl_-xsjFzijUDNM=",
+          }}
+        />
+      </View>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> to guess the number{" "}
+          <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+      </View>
+      <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    color: "black",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold',
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15,
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20,
+  }
+});
 
 export default GameOverScreen;
